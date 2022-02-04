c9e393b5471f31dad37c661f2d6412453085277c
jwun95 <jwun95@naver.com>
2022-02-05 03:15:32 +0900

    프로젝트 완료

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,13 +2,12 @@ const menuBtn = document.querySelector('.menu-btn');
 const hamburger = document.querySelector('.menu-btn__burger');
 const nav = document.querySelector('.nav');
 const menuNav = document.querySelector('.menu-nav');
-const navItems = document.querySelectorAll('.menu-nav__items');
+const navItems = document.querySelectorAll('.menu-nav__item');
+
 
 let showMenu = false;
 
-menuBtn.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
+const toggleMenu = () => {
     if(!showMenu) {
         hamburger.classList.add('open');
         nav.classList.add('open');
@@ -25,3 +24,5 @@ function toggleMenu() {
         showMenu = false;
     }
 }
+
+menuBtn.addEventListener('click', toggleMenu);
